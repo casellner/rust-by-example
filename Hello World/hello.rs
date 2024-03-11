c3944cd53bc9ecd4409d29d1ca3844364ead6e1f
casellner <114782256+casellner@users.noreply.github.com>
2024-03-10 23:59:44 -0400

    Init commit for hello.rs and .gitignore

---
+++ Hello World/hello.rs
@@ -0,0 +1,11 @@
+/*
+    Filename: hello.rs
+    Author:   Rust By Example, modified by Colton Sellner
+    Date:     10 March 2024
+    Purpose:  Print to the screen, comment, and do math in Rust.
+*/
+
+fn main() {
+    //an exclamation mark is used since println is a macro
+    println!("Hello World!");
+}
